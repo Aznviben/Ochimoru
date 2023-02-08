d1b68e621b129619fea8cbe74dfd5cf87be83172
Magikro <majikrow@gmail.com>
2023-02-08 10:19:01 -0500

    Testing Text & Input

--- ochimoru/App.js
+++ ochimoru/App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+  const [name, setName] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View 
+      justifyContent={'center'}
+      alignItems={'center'}
+      flex={1}
+      >
+        <View>
+          <Text>
+            Ochimoru; the start of something beautiful
+          </Text>
+        </View>
+      </View>
+      <View
+      justifyContent={'center'}
+      alignItems={'center'}
+      flex={1}
+      >
+        <TextInput
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        style={{textAlign:'center',fontSize:50}}
+        placeholder={"Please enter \nyour \nOchimoru's \nName"}
+        onChangeText={newName => setName(newName)}
+        defaultValue={name}
+        autoFocus={true}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
